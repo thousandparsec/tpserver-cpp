53eea293a7b7352492e532aee4cbe01a964eece6
Lee Begg <lee@thousandparsec.net>
2004-10-03 01:53:57 +0000

    Fixed speeds, and make them 10 times faster too.

--- src/fleet.cpp
+++ src/fleet.cpp
@@ -34,11 +34,11 @@ int Fleet::numShips(int type){
 
 long long Fleet::maxSpeed(){
   if(ships[2] > 0){
-    return 10000000;
+    return 100000000;
   }else if(ships[1] > 0){
-    return 20000000;
+    return 200000000;
   }else{
-    return 40000000;
+    return 300000000;
   }
 }
 
