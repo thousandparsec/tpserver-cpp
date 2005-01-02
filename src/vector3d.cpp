e37f75e9141382028d808b5f1f7f101593321331
Lee Begg <lee@thousandparsec.net>
2005-01-02 11:50:19 +0000

    Fixed many bugs thanks to Valgrind.

--- src/vector3d.cpp
+++ src/vector3d.cpp
@@ -1,3 +1,22 @@
+/*  3D vector with long long components
+ *
+ *  Copyright (C) 2004  Lee Begg and the Thousand Parsec Project
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
 
 #include <math.h>
 
@@ -6,7 +25,7 @@
 #include "vector3d.h"
 
 Vector3d::Vector3d(){
-
+  x = y = z = 0LL;
 }
 
 Vector3d::Vector3d(long long newx, long long newy, long long newz){
