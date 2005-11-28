55f6d7768b5780e440ff96b2e2d8c0bffe693efc
Lee Begg <lee@thousandparsec.net>
2005-11-28 05:03:43 +0000

    Add ObjectManager class to be a cache of objects.

---
+++ src/objectmanager.cpp
@@ -0,0 +1,134 @@
+/*  ObjectManager class
+ *
+ *  Copyright (C) 2005  Lee Begg and the Thousand Parsec Project
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+#include <time.h>
+
+#include "persistence.h"
+#include "game.h"
+#include "object.h"
+
+#include "objectmanager.h"
+
+ObjectManager::ObjectManager() : nextid(0){
+}
+
+ObjectManager::~ObjectManager(){
+    // sync to persistence
+}
+
+IGObject* ObjectManager::createNewObject(){
+    if(nextid == 0){
+        nextid = Game::getGame()->getPersistence()->getMaxObjectId();
+        if(nextid != 0)
+            nextid++;
+    }
+    IGObject* obj = new IGObject();
+    obj->setID(nextid++);
+    return obj;
+}
+
+void ObjectManager::addObject(IGObject* obj){
+    objects[obj->getID()] = obj;
+    if(obj->getID() == 0){
+        universe = obj;
+    }
+    Game::getGame()->getPersistence()->saveObject(obj);
+}
+
+void ObjectManager::discardNewObject(IGObject* obj){
+    if(obj->getID() == nextid - 1){
+        nextid = obj->getID();
+    }
+    delete obj;
+}
+
+IGObject *ObjectManager::getObject(uint32_t id){
+    if (id == 0) {
+        return universe;
+    }
+    IGObject *rtn = NULL;
+    std::map < unsigned int, IGObject * >::iterator obj = objects.find(id);
+    if (obj != objects.end()) {
+        rtn = (*obj).second;
+    }
+    if(rtn == NULL){
+        rtn = Game::getGame()->getPersistence()->retrieveObject(id);
+        if(rtn != NULL)
+            objects[id] = rtn;
+    }
+    return rtn;
+    //may need more work
+}
+
+void doneWithObject(uint32_t id){
+}
+
+void ObjectManager::scheduleRemoveObject(uint32_t id){
+    scheduleRemove.insert(id);
+}
+
+void ObjectManager::clearRemovedObjects(){
+    for(std::set<unsigned int>::iterator itrm = scheduleRemove.begin(); itrm != scheduleRemove.end(); ++itrm){
+        objects[*itrm]->removeFromParent();
+        // remove from persistence
+        delete objects[*itrm];
+        objects.erase(*itrm);
+    }
+    scheduleRemove.clear();
+}
+
+std::set<uint32_t> ObjectManager::getObjectsByPos(const Vector3d & pos, uint64_t r){
+    std::set <uint32_t> oblist;
+
+    for(std::map<uint32_t, IGObject *>::iterator itcurr = objects.begin(); itcurr != objects.end(); ++itcurr) {
+        uint64_t br = itcurr->second->getSize() / 2;
+        uint64_t diff = itcurr->second->getPosition().getDistance(pos); /*- r - br;*/
+        if(diff <=  r + br)
+        oblist.insert(itcurr->first);
+    
+    }
+    
+    return oblist;
+}
+
+std::set<uint32_t> ObjectManager::getContainerByPos(const Vector3d & pos){
+    std::set<uint32_t> oblist;
+
+    for(std::map<uint32_t, IGObject *>::iterator itcurr = objects.begin(); itcurr != objects.end(); ++itcurr){
+        if(itcurr->second->getContainerType() >= 1){
+        uint64_t br = itcurr->second->getSize() / 2 + itcurr->second->getSize() % 2;
+        
+        //long long diff = itcurr->second->getPosition().getDistanceSq(pos) - br * br;
+        if((uint64_t)(itcurr->second->getPosition().getDistance(pos)) <= br)
+            oblist.insert(itcurr->first);
+        }
+    }
+    
+    return oblist;
+}
+
+std::set<uint32_t> ObjectManager::getAllIds(){
+    std::set<unsigned int> vis;
+    for(std::map<uint32_t, IGObject*>::const_iterator itid = objects.begin();
+        itid != objects.end(); ++itid){
+        vis.insert(itid->first);
+    }
+    return vis;
+}
---
+++ src/objectmanager.h
@@ -0,0 +1,58 @@
+#ifndef OBJECTMANAGER_H
+#define OBJECTMANAGER_H
+/* ObjectManager class, looks after the objects that are in play
+ *
+ *  Copyright (C) 2005  Lee Begg and the Thousand Parsec Project
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+#include <map>
+#include <set>
+
+class IGObject;
+class Vector3d;
+
+class ObjectManager{
+ public:
+    ObjectManager();
+    ~ObjectManager();
+
+    IGObject* createNewObject();
+    void addObject(IGObject* obj);
+    void discardNewObject(IGObject* obj);
+
+    IGObject *getObject(uint32_t id);
+    void doneWithObject(uint32_t id);
+
+    void scheduleRemoveObject(uint32_t id);
+    void clearRemovedObjects();
+    
+    std::set<uint32_t> getObjectsByPos(const Vector3d & pos, uint64_t r);
+    std::set<uint32_t> getContainerByPos(const Vector3d & pos);
+    
+    std::set<uint32_t> getAllIds();
+
+ private:
+    IGObject *universe;
+    std::map<uint32_t, IGObject *> objects;
+    std::map<uint32_t, uint64_t> objmtime;
+    std::map<uint32_t, uint32_t> objatime;
+    std::set<uint32_t> scheduleRemove;
+    uint32_t nextid;
+};
+
+#endif
