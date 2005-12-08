57193c4e8f9f0096b38bf4470070a5ca2df8f4f8
Lee Begg <lee@thousandparsec.net>
2005-12-08 06:23:28 +0000

    Updated copyright dates.

--- src/colonise.h
+++ src/colonise.h
@@ -2,7 +2,7 @@
 #define COLONISE_H
 /*  Colonise Order
  *
- *  Copyright (C) 2004  Lee Begg and the Thousand Parsec Project
+ *  Copyright (C) 2004-2005  Lee Begg and the Thousand Parsec Project
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
