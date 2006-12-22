7c8ad172d37c28b58310df0ae40824cdef7c50bb
Lee Begg <lee@thousandparsec.net>
2006-12-22 09:54:34 +0000

    Again with the copyright dates.

--- tpserver/settings.cpp
+++ tpserver/settings.cpp
@@ -1,6 +1,6 @@
 /*  Setting class
  *
- *  Copyright (C) 2003-2005  Lee Begg and the Thousand Parsec Project
+ *  Copyright (C) 2003-2005, 2006  Lee Begg and the Thousand Parsec Project
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
