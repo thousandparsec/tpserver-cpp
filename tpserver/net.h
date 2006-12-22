fac934b90682e9760138c79e9666e01d70b21220
Lee Begg <lee@thousandparsec.net>
2006-12-22 09:08:34 +0000

    Update copyright dates for recently modified files.
    I keep forgetting to do this. Will try to remember.
    .

--- tpserver/net.h
+++ tpserver/net.h
@@ -2,7 +2,7 @@
 #define NET_H
 /*  Network Abstraction class
  *
- *  Copyright (C) 2004-2005  Lee Begg and the Thousand Parsec Project
+ *  Copyright (C) 2004-2005, 2006  Lee Begg and the Thousand Parsec Project
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
