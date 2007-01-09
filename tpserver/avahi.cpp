c3dbf1b46ff344ae1334b11ff7a94e6f17003d71
Lee Begg <lee@thousandparsec.net>
2007-01-09 10:06:11 +0000

    It is hard to remember to update copyright dates just after new years.

--- tpserver/avahi.cpp
+++ tpserver/avahi.cpp
@@ -1,6 +1,6 @@
 /*  Avahi mDNS-SD abstraction for tpserver-cpp
  *
- *  Copyright (C) 2006  Lee Begg and the Thousand Parsec Project
+ *  Copyright (C) 2006, 2007  Lee Begg and the Thousand Parsec Project
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
