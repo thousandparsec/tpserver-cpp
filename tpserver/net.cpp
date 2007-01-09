66ef071992aba99d886a093625c94b44e538b2e2
Lee Begg <lee@thousandparsec.net>
2007-01-09 09:46:16 +0000

    Again with the copyright dates, this time net.cpp and advertiser.h.

--- tpserver/net.cpp
+++ tpserver/net.cpp
@@ -1,6 +1,6 @@
 /*  Network abstraction for tpserver-cpp 
  *
- *  Copyright (C) 2003-2005, 2006  Lee Begg and the Thousand Parsec Project
+ *  Copyright (C) 2003-2005, 2006, 2007  Lee Begg and the Thousand Parsec Project
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
