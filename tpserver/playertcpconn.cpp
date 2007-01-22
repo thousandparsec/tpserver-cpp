eb9d84ea308b2452c94ad17fcc99a348bee12815
Lee Begg <lee@thousandparsec.net>
2007-01-22 13:38:50 +0000

    Fixed verCheck for future non-blocking sockets.
    This means that tp sockets (PlayerTcpConnection) can handle non-blocking sockets, but
    http (PlayerHttpConnection) which has it's own verCheck, but uses PlayerTcpConnection
    for everything else. This will be fixed shortly. Then non-blocking sockets will be
    turned on for these two sockets.
    .

--- tpserver/playertcpconn.cpp
+++ tpserver/playertcpconn.cpp
@@ -1,6 +1,6 @@
 /*  Player Tcp Connection object, supports ipv4 and ipv6
  *
- *  Copyright (C) 2003-2005  Lee Begg and the Thousand Parsec Project
+ *  Copyright (C) 2003-2005, 2007  Lee Begg and the Thousand Parsec Project
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -98,137 +98,125 @@ void PlayerTcpConnection::sendFrame(Frame * frame)
 
 
 
-void PlayerTcpConnection::verCheck()
-{
-	// FIXME: This is bad and should be fixed!
-	char *buff = new char[5];
-	int len = read(sockfd, buff, 4);
-	buff[4] = '\0';
-	
-	if (len == 4 && memcmp(buff, "TP", 2) == 0){
-	  // assume we are talking TPprotocol
-	  
-	  int ver = atoi(buff+2);
-	  if(ver == 1){
-	    Logger::getLogger()->warning("Client did not show correct version of protocol (version 1)");
-	    send(sockfd, "You are not running the right version of TP, please upgrade\n", 60, 0);
-	    close();
-	  }else if(ver > 3){
-	    Logger::getLogger()->warning("Client has higher version (%d), telling it so", ver);
-	    Frame *f = new Frame(fv0_3);
-	    f->setSequence(0);
-	    f->createFailFrame(fec_ProtocolError, "TP Protocol, but I only support versions 2 and 3, sorry.");
-	    sendFrame(f);
-	    //stay connected just in case they try again with a lower version
-	    // have to empty the receive queue though.
-	    delete[] buff;
-	    buff = new char[1024];
-	    len = read(sockfd, buff, 1024);
-	    Logger::getLogger()->debug("Read an extra %d bytes from the socket, into buffer of 1024", len);
-	  }else{
-	    
-	    // check the rest of the packet
-	    delete[] buff;
-	    buff = new char[12];
-	    len = read(sockfd, buff, 12);
-	    
-	    if (len == 12) {
-	      // Sequence number
-	      int seqNum = 0, nseqNum;
-	      memcpy(&nseqNum, buff, 4);
-	      seqNum = ntohl(nseqNum);
-	      
-	      // Type of packet
-	      //since tp03 allows Get Feature frames before a connect frame, the first
-	      // frame could be a get feature.
-	      int typeNum = 0, ntypeNum;
-	      memcpy(&ntypeNum, buff + 4, 4);
-	      typeNum = ntohl(ntypeNum);
+void PlayerTcpConnection::verCheck(){
 
-	      // Length of packet
-	      int lenNum = 0, nlenNum;
-	      memcpy(&nlenNum, buff + 8, 4);
-	      lenNum = ntohl(nlenNum);
-	      
-	      if(ver == 3 && typeNum == ft03_Features_Get){
-		version = fv0_3;
-		if(lenNum != 0){
-		  // ver 3 frame error, Get Features have no data.
-		  Logger::getLogger()->warning("Invalid Get Features request");
+  bool rtn = true;
+  Frame *recvframe = new Frame(fv0_3);
+  uint32_t hlen = recvframe->getHeaderLength();
+  
+  if(rheaderbuff == NULL){
+    rbuffused = 0;
+    rheaderbuff = new char[hlen];
+  }
+  
+  if(rdatabuff == NULL && rbuffused < 4){
+    uint32_t len = recv(sockfd, rheaderbuff+rbuffused, 4, 0);
+    if(len == 0){
+      Logger::getLogger()->info("Client disconnected");
+      close();
+      rtn = false;
+    }else if(len > 0){
+      rbuffused += len;
+      if(rbuffused < 4){
+        Logger::getLogger()->debug("ver check header not the length needed, delaying read");
+        rtn = false;
+      }
+    }else{
+      if(errno != EAGAIN && errno != EWOULDBLOCK){
+        Logger::getLogger()->warning("Socket error");
+        close();
+      }
+      rtn = false;
+    }
+  }
+  if(rtn && rdatabuff == NULL && rbuffused >= 4){
+    if(rheaderbuff[0] == 'T' && rheaderbuff[1] == 'P'){
+      if(rheaderbuff[2] == '0'){
+        if(rheaderbuff[3] <= '1'){
+          Logger::getLogger()->warning("Client did not show correct version of protocol (version 1)");
+          send(sockfd, "You are not running the right version of TP, please upgrade\n", 60, 0);
+          close();
+          rtn = false;
+        }else if(rheaderbuff[3] > '3'){
+          //might be future version of protocol, just disconnect now
+          Frame *f = new Frame(fv0_3);
+          f->setSequence(0);
+          f->createFailFrame(fec_ProtocolError, "TP Protocol, but I only support versions 2 and 3, sorry.");
+          sendFrame(f);
+  
+          //stay connected just in case they try again with a lower version
+          // have to empty the receive queue though.
+          char* buff = new char[1024];
+          uint32_t len = recv(sockfd, buff, 1024, 0);
+          Logger::getLogger()->debug("Read an extra %d bytes from the socket, into buffer of 1024", len);
+          delete[] buff;
+          rtn = false;
+        }else{
+          version = (FrameVersion)atoi(rheaderbuff+2);
+        }
+      }else{
+        //might be future version of protocol, just disconnect now
+        Frame *f = new Frame(fv0_3);
+        f->setSequence(0);
+        f->createFailFrame(fec_ProtocolError, "TP Protocol, but I only support versions 2 and 3, sorry.");
+        sendFrame(f);
 
-		  // remove the data from the socket
-		  char *cbuff = new char[lenNum];
-		  len = read(sockfd, cbuff, lenNum);
-		  delete[] cbuff;
+        //stay connected just in case they try again with a lower version
+        // have to empty the receive queue though.
+        char* buff = new char[1024];
+        uint32_t len = recv(sockfd, buff, 1024, 0);
+        Logger::getLogger()->debug("Read an extra %d bytes from the socket, into buffer of 1024", len);
+        delete[] buff;
+        rtn = false;
+      }
+      
+      if(rtn){
+        Logger::getLogger()->info("Client has version %d of protocol", version);
+        if(version != recvframe->getVersion()){
+          delete recvframe;
+          recvframe = new Frame(version);
+        }
+        if(readFrame(recvframe)){
+          if(recvframe->getType() == ft02_Connect){
+            char* clientsoft = recvframe->unpackString();
+            Logger::getLogger()->info("Client on connection %d is [%s]", sockfd, clientsoft);
+            delete[] clientsoft;
+            
+            status = 2;
+            
+            Frame *okframe = createFrame(recvframe);
+            okframe->setType(ft02_OK);
+            
+            okframe->packString("Protocol check ok, continue! Welcome to tpserver-cpp " VERSION);
+            sendFrame(okframe);
+          }else if(recvframe->getVersion() >= 3 && recvframe->getType() == ft03_Features_Get){
+            Logger::getLogger()->debug("Get Features request");
+            Frame* features = createFrame(recvframe);
 
-		  Frame* fe = createFrame(NULL);
-		  fe->setSequence(seqNum);
-		  fe->createFailFrame(fec_FrameError, "GetFeatures frame should have no data");
-		  sendFrame(fe);
-		}else{
-		  Logger::getLogger()->debug("Get Features request");
-		  Frame* features = createFrame(NULL);
-		  features->setSequence(seqNum);
+            Network::getNetwork()->createFeaturesFrame(features);
 
-		  Network::getNetwork()->createFeaturesFrame(features);
-
-		  sendFrame(features);
-		}
-		
-		delete[] buff;
-		return;
-	      }
-	      // Read in the length of the packet and ignore
-	      char *cbuff = new char[lenNum + 1];
-	      len = read(sockfd, cbuff, lenNum);
-                cbuff[lenNum] = '\0';
-
-            if(typeNum == ft02_Connect){
-		
-		Logger::getLogger()->info("Client on connection %d is [%s]", sockfd, cbuff + 4);
-		
-		version = (FrameVersion)ver;
-
-		Logger::getLogger()->info("Client has version %d of protocol", version);
-		
-		status = 2;
-		
-		Frame *okframe = createFrame(NULL);
-		// since we don't create a frame object, we have to set the sequence number seperately
-		okframe->setSequence(seqNum);
-		okframe->setType(ft02_OK);
-		
-		okframe->packString("Protocol check ok, continue! Welcome to tpserver-cpp " VERSION);
-		sendFrame(okframe);
-	      }else{
-		Logger::getLogger()->warning("First frame wasn't Connect or GetFeatures, was %d", typeNum);
-		Frame* fe = createFrame(NULL);
-		fe->setSequence(seqNum);
-		fe->createFailFrame(fec_ProtocolError, "First frame wasn't Connect (or GetFeatures in tp03), please try again");
-		sendFrame(fe);
-	      }
-	      delete[] cbuff;
-	    }
-	  }
-	  delete[] buff;
-	  return;
-	  
-	}
-	Logger::getLogger()->warning("Client did not talk any variant of TPprotocol");
-	// send "I don't understand" message
-	if (len != 0) {
-	  /*Frame *failframe = new Frame();
-	  failframe->createFailFrame(0, "You are not running the correct protocol");	// TODO - should be a const or enum, protocol error
-	  sendFrame(failframe);*/
-
-	  send(sockfd, "You are not running the correct protocol\n", 41, 0);
-
-	}
-	close();
-
-	delete[]buff;
+            sendFrame(features);
+          }else{
+            Logger::getLogger()->warning("First frame wasn't Connect or GetFeatures, was %d", recvframe->getType());
+            Frame* fe = createFrame(recvframe);
+            fe->createFailFrame(fec_ProtocolError, "First frame wasn't Connect (or GetFeatures in tp03), please try again");
+            sendFrame(fe);
+          }
+        }else{
+          rtn = false;
+        }
+      }
+    }else{
+      Logger::getLogger()->warning("Client did not talk any variant of TPprotocol");
+      // send "I don't understand" message
+      send(sockfd, "You are not running the correct protocol\n", 41, 0);
+      close();
+      rtn = false;
+    }
+  }else{
+    rtn = false;
+  }
 }
-
 
 bool PlayerTcpConnection::readFrame(Frame * recvframe)
 {
