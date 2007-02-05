86fa7bf32b2437577d9a183e2cc0eec314d98a28
Lee Begg <lee@thousandparsec.net>
2007-02-05 10:28:59 +0000

    Added Mersenne Twister (mt19937), for PRNG.
    Borrowed from http://www.math.sci.hiroshima-u.ac.jp/~m-mat/MT/MT2002/emt19937ar.html (BSD type
    license)
    Needs some clean up and more useful generators.
    .

---
+++ tpserver/prng.h
@@ -0,0 +1,93 @@
+/* 
+   A C-program for MT19937, with initialization improved 2002/1/26.
+   Coded by Takuji Nishimura and Makoto Matsumoto.
+
+   Before using, initialize the state by using init_genrand(seed)  
+   or init_by_array(init_key, key_length).
+
+   Copyright (C) 1997 - 2002, Makoto Matsumoto and Takuji Nishimura,
+   All rights reserved.
+   Copyright (C) 2005, Mutsuo Saito
+   All rights reserved.
+   Copyright (C) 2007, Lee Begg and the Thousand Parsec Project
+   (C++-fying)
+
+   Redistribution and use in source and binary forms, with or without
+   modification, are permitted provided that the following conditions
+   are met:
+
+     1. Redistributions of source code must retain the above copyright
+        notice, this list of conditions and the following disclaimer.
+
+     2. Redistributions in binary form must reproduce the above copyright
+        notice, this list of conditions and the following disclaimer in the
+        documentation and/or other materials provided with the distribution.
+
+     3. The names of its contributors may not be used to endorse or promote 
+        products derived from this software without specific prior written 
+        permission.
+
+   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+   A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+   CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+   EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+   PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+   PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+   LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+   NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
+   Any feedback is very welcome.
+   http://www.math.sci.hiroshima-u.ac.jp/~m-mat/MT/emt.html
+   email: m-mat @ math.sci.hiroshima-u.ac.jp (remove space)
+*/
+
+#ifndef PRNG_H
+#define PRNG_H
+
+class Random{
+  public:
+    Random();
+    ~Random();
+    
+    /* initializes mt[N] with a seed */
+    void seed(unsigned long s);
+    
+    /* initialize by an array with array-length */
+    /* init_key is the array for initializing keys */
+    /* key_length is its length */
+    /* slight change for C++, 2004/2/26 */
+    void seed(unsigned long init_key[], int key_length);
+    
+    /* generates a random number on [0,0xffffffff]-interval */
+    unsigned long getInt32(void);
+    
+    /* generates a random number on [0,0x7fffffff]-interval */
+    long getInt31(void);
+    
+    /* These real versions are due to Isaku Wada, 2002/01/09 added */
+    /* generates a random number on [0,1]-real-interval */
+    double getReal1(void);
+    
+    /* generates a random number on [0,1)-real-interval */
+    double getReal2(void);
+    
+    /* generates a random number on (0,1)-real-interval */
+    double getReal3(void);
+    
+    /* generates a random number on [0,1) with 53-bit resolution*/
+    double getRes53(void);
+    
+  private:
+
+    unsigned long mt[624]; /* the array for the state vector  */
+    int mti; /* mti==N+1 means mt[N] is not initialized */
+
+
+};
+
+#endif
+
