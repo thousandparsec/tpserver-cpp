1895d0bbc791df4c89cc9b210fc6da326d3d4a36
Lee Begg <lee@thousandparsec.net>
2007-02-18 01:30:47 +0000

    Removed compile warning, and get error type on failed read.
    So now read and write both record the failure error.
    Just a newline at the end of the file to get rid of the compile warning.
    .

--- tpserver/playertcpconn.cpp
+++ tpserver/playertcpconn.cpp
@@ -428,6 +428,7 @@ int32_t PlayerTcpConnection::underlyingRead(char* buff, uint32_t size){
   int32_t len = recv(sockfd, buff, size, 0);
   if(len < 0){
     if(errno != EAGAIN && errno != EWOULDBLOCK){
+      Logger::getLogger()->error("underlying read, tcp, error is: %s", strerror(errno));
       len = -1;
     }else{
       len = -2;
