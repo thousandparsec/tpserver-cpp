24264449766759a5d9adbb5c1bfe3242eb02c7fe
Lee Begg <lee@thousandparsec.net>
2007-02-18 07:32:14 +0000

    Rspcombat copyright.

--- modules/games/minisec/rspcombat.cpp
+++ modules/games/minisec/rspcombat.cpp
@@ -1,6 +1,6 @@
 /*  Rock-Scissor-Paper combat
  *
- *  Copyright (C) 2004-2005  Lee Begg and the Thousand Parsec Project
+ *  Copyright (C) 2004-2005, 2007  Lee Begg and the Thousand Parsec Project
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
