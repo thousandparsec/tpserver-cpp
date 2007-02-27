3dd88b6c599c8daf5dcac2385ebe87e51960f36c
Lee Begg <lee@thousandparsec.net>
2007-02-27 11:10:43 +0000

    Added missing stdint.h include in player.h.

--- tpserver/player.h
+++ tpserver/player.h
@@ -2,7 +2,7 @@
 #define PLAYER_H
 /*  Player class
  *
- *  Copyright (C) 2004-2005  Lee Begg and the Thousand Parsec Project
+ *  Copyright (C) 2004-2005, 2007  Lee Begg and the Thousand Parsec Project
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -22,6 +22,7 @@
 
 #include <set>
 #include <string>
+#include <stdint.h>
 
 class PlayerConnection;
 class Frame;
