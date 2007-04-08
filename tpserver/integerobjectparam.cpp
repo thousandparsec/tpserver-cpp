164a65ca0b15f6d7928d1231c40e186656def02b
Lee Begg <lee@thousandparsec.net>
2007-04-08 10:39:57 +0000

    Added size and integer object parameters.
    This is now enough for the Universe, Galaxy and star systems in Minisec.
    .

---
+++ tpserver/integerobjectparam.cpp
@@ -0,0 +1,59 @@
+/*  IntegerObjectParam baseclass
+ *
+ *  Copyright (C) 2007 Lee Begg and the Thousand Parsec Project
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+#include "frame.h"
+
+#include "integerobjectparam.h"
+
+IntegerObjectParam::IntegerObjectParam() : ObjectParameter(), value(0){
+  type = obpT_Integer;
+}
+
+IntegerObjectParam::~IntegerObjectParam(){
+
+}
+
+
+void IntegerObjectParam::packObjectFrame(Frame * f, uint32_t objID){
+  f->packInt(value);
+}
+
+
+bool IntegerObjectParam::unpackModifyObjectFrame(Frame *f, unsigned int playerid){
+  // all fields are read only
+  if(!f->isEnoughRemaining(4))
+    return false;
+  //value = f->unpackInt();
+  f->setUnpackOffset(f->getUnpackOffset() + 4);
+  return true;
+}
+
+ObjectParameter* IntegerObjectParam::clone() const{
+  return new IntegerObjectParam();
+}
+
+uint32_t IntegerObjectParam::getValue() const{
+  return value;
+}
+
+void IntegerObjectParam::setValue(uint32_t nv){
+  value = nv;
+}
+
---
+++ tpserver/sizeobjectparam.h
@@ -0,0 +1,46 @@
+#ifndef SIZEOBJECTPARAM_H
+#define SIZEOBJECTPARAM_H
+/*  SizeObjectParam class
+ *
+ *  Copyright (C) 2007  Lee Begg and the Thousand Parsec Project
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+#include <stdint.h>
+
+#include <tpserver/objectparameter.h>
+
+class SizeObjectParam : public ObjectParameter{
+
+public:
+  SizeObjectParam();
+  virtual ~SizeObjectParam();
+
+  virtual void packObjectFrame(Frame * f, uint32_t objID);
+  virtual bool unpackModifyObjectFrame(Frame * f, unsigned int playerid);
+
+  virtual ObjectParameter *clone() const;
+  
+  uint64_t getSize() const;
+  void setSize(uint64_t ns);
+
+protected:
+  uint64_t size;
+
+};
+
+#endif
