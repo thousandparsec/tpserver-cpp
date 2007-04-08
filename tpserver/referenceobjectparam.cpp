739bfe98e511b1ccd43fa51c80f5df71d5a2de68
Lee Begg <lee@thousandparsec.net>
2007-04-08 11:01:06 +0000

    Added Reference Object Parameter.
    Used for holding the owner's player id.
    .

---
+++ tpserver/referenceobjectparam.cpp
@@ -0,0 +1,69 @@
+/*  ReferenceObjectParam baseclass
+ *
+ *  Copyright (C) 2007 Lee Begg and the Thousand Parsec Project
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+#include "frame.h"
+
+#include "referenceobjectparam.h"
+
+ReferenceObjectParam::ReferenceObjectParam() : ObjectParameter(), reftype(0), refid(0){
+  type = obpT_Reference;
+}
+
+ReferenceObjectParam::~ReferenceObjectParam(){
+
+}
+
+
+void ReferenceObjectParam::packObjectFrame(Frame * f, uint32_t objID){
+  f->packInt(reftype);
+  f->packInt(refid);
+}
+
+
+bool ReferenceObjectParam::unpackModifyObjectFrame(Frame *f, unsigned int playerid){
+  // all fields are read only
+  if(!f->isEnoughRemaining(8))
+    return false;
+  //reftype = f->unpackInt();
+  //refid = f->unpackInt();
+  f->setUnpackOffset(f->getUnpackOffset() + 8);
+  return true;
+}
+
+ObjectParameter* ReferenceObjectParam::clone() const{
+  return new ReferenceObjectParam();
+}
+
+int32_t ReferenceObjectParam::getReferenceType() const{
+  return reftype;
+}
+
+void ReferenceObjectParam::setReferenceType(int32_t nt){
+  reftype = nt;
+}
+
+uint32_t ReferenceObjectParam::getReferencedId() const{
+  return refid;
+}
+
+void ReferenceObjectParam::setReferencedId(uint32_t ni){
+  refid = ni;
+}
+
---
+++ tpserver/referenceobjectparam.h
@@ -0,0 +1,50 @@
+#ifndef REFERENCEOBJECTPARAM_H
+#define REFERECEOBJECTPARAM_H
+/*  ReferenceObjectParam class
+ *
+ *  Copyright (C) 2007  Lee Begg and the Thousand Parsec Project
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+#include <stdint.h>
+
+#include <tpserver/objectparameter.h>
+
+class ReferenceObjectParam : public ObjectParameter{
+
+public:
+  ReferenceObjectParam();
+  virtual ~ReferenceObjectParam();
+
+  virtual void packObjectFrame(Frame * f, uint32_t objID);
+  virtual bool unpackModifyObjectFrame(Frame * f, unsigned int playerid);
+
+  virtual ObjectParameter *clone() const;
+  
+  int32_t getReferenceType() const;
+  void setReferenceType(int32_t nt);
+  
+  uint32_t getReferencedId() const;
+  void setReferencedId(uint32_t ni);
+
+protected:
+  int32_t reftype;
+  uint32_t refid;
+
+};
+
+#endif
