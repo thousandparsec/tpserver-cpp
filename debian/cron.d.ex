a1eadb411580d58f90c5aaac386fcd4c38c4aa93
Paul Schulz <paul@mawsonlakes.org>
2007-05-22 21:39:47 +0930

    Initial dh_make debian step

---
+++ debian/cron.d.ex
@@ -0,0 +1,4 @@
+#
+# Regular cron jobs for the tpserver-cpp package
+#
+0 4	* * *	root	tpserver-cpp_maintenance
---
+++ debian/menu.ex
@@ -0,0 +1,2 @@
+?package(tpserver-cpp):needs="X11|text|vc|wm" section="Apps/see-menu-manual"\
+  title="tpserver-cpp" command="/usr/bin/tpserver-cpp"
