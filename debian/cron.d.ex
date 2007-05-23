cf75c9c52f465d9d024e691cac1c1cc79e02af9a
Paul Schulz <paul@mawsonlakes.org>
2007-05-23 23:41:17 +0930

    Create multiple debian packages

--- debian/cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the tpserver-cpp package
-#
-0 4	* * *	root	tpserver-cpp_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(tpserver-cpp):needs="X11|text|vc|wm" section="Apps/see-menu-manual"\
-  title="tpserver-cpp" command="/usr/bin/tpserver-cpp"
