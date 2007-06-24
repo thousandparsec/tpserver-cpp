d78818f5633195ed3c7782a15e57399abd50888a
Tyler Shaub <fr33.em4il@gmail.com>
2007-06-24 00:04:46 -0700

    Adding planet class

---
+++ modules/games/rfts/planet.cpp
@@ -0,0 +1,196 @@
+/*  Planet
+ *
+ *  Copyright (C) 2007  Tyler Shaub and the Thousand Parsec Project
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+#include <map>
+
+#include <tpserver/frame.h>
+#include <tpserver/position3dobjectparam.h>
+#include <tpserver/sizeobjectparam.h>
+#include <tpserver/refsys.h>
+#include <tpserver/referenceobjectparam.h>
+#include <tpserver/resourcelistobjectparam.h>
+#include <tpserver/position3dobjectparam.h>
+#include <tpserver/sizeobjectparam.h>
+#include <tpserver/orderqueueobjectparam.h>
+#include <tpserver/objectparametergroup.h>
+
+#include "containertypes.h"
+
+#include "planet.h"
+
+namespace RFTS_ {
+
+Planet::Planet() : ObjectData() {
+   nametype = "Planet";
+   typedesc = "A planet object";
+
+   pos = new Position3dObjectParam();
+   pos->setName("Position");
+   pos->setDescription("The position of the planet");
+   ObjectParameterGroup* group = new ObjectParameterGroup();
+   group->setGroupId(1);
+   group->setName("Positional");
+   group->setDescription("Positional information");
+   group->addParameter(pos);
+   size = new SizeObjectParam();
+   size->setName("Size");
+   size->setDescription( "The size of the planet");
+   size->setSize(2);
+   group->addParameter(size);
+   paramgroups.push_back(group);
+   
+   playerref = new ReferenceObjectParam();
+   playerref->setName("Owner");
+   playerref->setDescription("The owner of this object");
+   playerref->setReferenceType(rst_Player);
+   group = new ObjectParameterGroup();
+   group->setGroupId(2);
+   group->setName("Ownership");
+   group->setDescription("The ownership of this object");
+   group->addParameter(playerref);
+   paramgroups.push_back(group);
+      
+   group = new ObjectParameterGroup();
+   group->setGroupId(3);
+   group->setName("Resources");
+   group->setDescription("The planets resources");
+   resources = new ResourceListObjectParam();
+   resources->setName("Resource List");
+   resources->setDescription("The resource list of the resources the planet has available");
+   group->addParameter(resources);
+   paramgroups.push_back(group);
+   
+   /* possibly not used
+   group = new ObjectParameterGroup();
+   group->setGroupId(4);
+   group->setName("Orders");
+   group->setDescription("The order queues of the planet");
+   orderqueue = new OrderQueueObjectParam();
+   orderqueue->setName("Order Queue");
+   orderqueue->setDescription("The queue of orders for this planet");
+   
+   group->addParameter(orderqueue);
+   paramgroups.push_back(group);
+   */
+ 
+}
+
+Planet::~Planet() {
+
+}
+
+void Planet::doOnceATurn(IGObject * obj) {
+
+}
+
+int Planet::getContainerType() {
+   return ContainerTypes_::PLANET;
+}
+
+ObjectData* Planet::clone() {
+   return new Planet();
+}
+
+void Planet::packExtraData(Frame * frame){
+  ObjectData::packExtraData(frame);
+  
+  frame->packInt((playerref->getReferencedId() == 0) ? 0xffffffff : playerref->getReferencedId());
+  
+  std::map<uint32_t, std::pair<uint32_t, uint32_t> > reslist = resources->getResources();
+    frame->packInt(reslist.size());
+    for(std::map<uint32_t, std::pair<uint32_t, uint32_t> >::iterator itcurr = reslist.begin();
+            itcurr != reslist.end(); ++itcurr){
+        frame->packInt(itcurr->first);
+        frame->packInt(itcurr->second.first);
+        frame->packInt(itcurr->second.second);
+        frame->packInt(0);
+    }
+}
+
+
+Vector3d Planet::getPosition() const{
+  return pos->getPosition();
+}
+
+uint64_t Planet::getSize() const{
+  return size->getSize();
+}
+
+void Planet::setPosition(const Vector3d & np){
+  pos->setPosition(np);
+  touchModTime();
+}
+
+void Planet::setSize(uint64_t ns){
+  size->setSize(ns);
+  touchModTime();
+}
+
+uint32_t Planet::getOwner() const{
+  return playerref->getReferencedId();
+}
+
+void Planet::setOwner(uint32_t no){
+  playerref->setReferencedId(no);
+  touchModTime();
+}
+
+std::map<uint32_t, std::pair<uint32_t, uint32_t> > Planet::getResources(){
+    return resources->getResources();
+}
+
+uint32_t Planet::getResource(uint32_t restype) const{
+  std::map<uint32_t, std::pair<uint32_t, uint32_t> > reslist = resources->getResources();
+  if(reslist.find(restype) != reslist.end()){
+    return reslist.find(restype)->first;
+  }
+  return 0;
+}
+
+void Planet::setResources(std::map<uint32_t, std::pair<uint32_t, uint32_t> > ress){
+    resources->setResources(ress);
+    touchModTime();
+}
+
+void Planet::addResource(uint32_t restype, uint32_t amount){
+  std::map<uint32_t, std::pair<uint32_t, uint32_t> > reslist = resources->getResources();
+    std::pair<uint32_t, uint32_t> respair = reslist[restype];
+    respair.first += amount;
+    reslist[restype] = respair;
+    resources->setResources(reslist);
+    touchModTime();
+}
+
+bool Planet::removeResource(uint32_t restype, uint32_t amount){
+  std::map<uint32_t, std::pair<uint32_t, uint32_t> > reslist = resources->getResources();
+    if(reslist.find(restype) != reslist.end()){
+        if(reslist[restype].first >= amount){
+            std::pair<uint32_t, uint32_t> respair = reslist[restype];
+            respair.first -= amount;
+            reslist[restype] = respair;
+            resources->setResources(reslist);
+            touchModTime();
+            return true;
+        }
+    }
+    return false;
+}
+
+}
---
+++ modules/games/rfts/planet.h
@@ -0,0 +1,73 @@
+#ifndef PLANET_H
+#define PLANET_H
+/*  Planet class
+ *
+ *  Copyright (C) 2007  Tyler Shaub and the Thousand Parsec Project
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+#include <tpserver/objectdata.h>
+#include <tpserver/vector3d.h>
+
+class Position3dObjectParam;
+class SizeObjectParam;
+class ReferenceObjectParam;
+class ResourceListObjectParam;
+class OrderQueueObjectParam;
+
+namespace RFTS_ {
+
+class Planet : public ObjectData {
+
+ public:
+   Planet();
+   virtual ~Planet();
+
+   virtual ObjectData* clone();
+
+   virtual void packExtraData(Frame * frame);
+   virtual void doOnceATurn(IGObject * obj);
+   virtual int getContainerType();
+   
+   Vector3d getPosition() const;
+   void setPosition(const Vector3d & np);
+
+   uint64_t getSize() const;
+   void setSize(uint64_t ns);
+        
+   uint32_t getOwner() const;
+   void setOwner(uint32_t no);
+
+   std::map<uint32_t, std::pair<uint32_t, uint32_t> > getResources();
+   uint32_t getResource(uint32_t restype) const;
+    
+   void setResources(std::map<uint32_t, std::pair<uint32_t, uint32_t> > ress);
+   void addResource(uint32_t restype, uint32_t amount);
+   bool removeResource(uint32_t restype, uint32_t amount);
+
+ private:
+   Position3dObjectParam * pos;
+   SizeObjectParam * size;
+   ReferenceObjectParam * playerref;
+   ResourceListObjectParam* resources;
+   //OrderQueueObjectParam * orderqueue; // might not need
+};
+
+
+}
+
+#endif
