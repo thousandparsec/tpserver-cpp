4506573f376b7e2f451af19995bdd76bd5990eb4
Lee Begg <llnz@paradise.net.nz>
2008-01-17 23:39:57 +1300

    IGObject now uses ObjectInfo/Relationships/ParameterGroupPtr/Behaviour. One of the largest changes in this effort to refactor Object for history and view support. Almost the same interface, though I suspect it can be trimed down a little bit. Only a few core things still broken, and all of the rulesets.

--- tpserver/object.cpp
+++ tpserver/object.cpp
@@ -1,6 +1,6 @@
 /*  In Game Objects in the game universe
  *
- *  Copyright (C) 2003-2005, 2007  Lee Begg and the Thousand Parsec Project
+ *  Copyright (C) 2003-2005, 2007, 2008  Lee Begg and the Thousand Parsec Project
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -36,239 +36,186 @@
 #include "orderqueueobjectparam.h"
 #include "ordermanager.h"
 #include "orderqueue.h"
+#include "objectbehaviour.h"
 
 #include "object.h"
 
-Game *IGObject::myGame = NULL;
 
-IGObject::IGObject() : name(), description()
-{
-	id = 0xffffffff;
-	parentid = 0;
-	if (myGame == NULL) {
-		myGame = Game::getGame();
-	}
-	myObjectData = NULL;
+IGObject::IGObject() : id(0xffffffff), turn(0), alive(true), info(), relationships(), parameters(),
+                   behaviour(NULL){
+  touchModTime();
 }
 
-IGObject::IGObject(IGObject & rhs){
+IGObject::IGObject(const IGObject & rhs){
   Logger::getLogger()->warning("Object Copy Constructor: copying Object ID");
 }
 
-IGObject::~IGObject()
-{
-	children.clear();
-	if(myObjectData != NULL){
-	  delete myObjectData;
-	}
+IGObject::~IGObject(){
+  parameters.clear();
+  if(behaviour != NULL){
+    delete behaviour;
+  }
 }
 
-IGObject IGObject::operator=(IGObject & rhs)
-{
-	//TODO
-        Logger::getLogger()->warning("Object Assignment operator");
-	return *this;
+IGObject& IGObject::operator=(const IGObject & rhs){
+  //TODO
+  Logger::getLogger()->warning("Object Assignment operator");
+  return *this;
 }
 
-unsigned int IGObject::getID()
-{
-	return id;
+uint32_t IGObject::getID() const{
+  return id;
 }
 
-unsigned int IGObject::getType()
-{
-	return type;
+uint32_t IGObject::getType() const{
+  return info->getType();
 }
 
-std::string IGObject::getName(){
-  return name;
+std::string IGObject::getName() const{
+  return info->getName();
 }
 
-std::string IGObject::getDescription(){
-  return description;
+std::string IGObject::getDescription() const{
+  return info->getDescription();
 }
 
-bool IGObject::setID(unsigned int newid)
-{
-	//check to see if id is use
-	//if it is, then return false
-	//else
+bool IGObject::isAlive() const{
+  return alive;
+}
+
+uint32_t IGObject::getTurn() const{
+  return turn;
+}
+
+uint32_t IGObject::getParent() const{
+  return relationships->getParent();
+}
+
+void IGObject::setID(uint32_t newid){
   touchModTime();
-	id = newid;
-	return true;
+  id = newid;
 }
 
-unsigned int IGObject::getParent(){
-  return parentid;
-}
-
-void IGObject::setType(unsigned int newtype)
-{
-	type = newtype;
-
-	if(myObjectData != NULL)
-	  delete myObjectData;
-
-	myObjectData = myGame->getObjectDataManager()->createObjectData(type);
-	if(myObjectData == NULL){
-	  Logger::getLogger()->warning("Object type not found");
-	  myObjectData = NULL;
-	  
-	}
-	touchModTime();
+void IGObject::setType(uint32_t newtype){
+  info->setType(newtype);
+  touchModTime();
 }
 
 void IGObject::setName(const std::string &newname){
-  name = newname;
+  info->setName(newname);
   touchModTime();
 }
 
 void IGObject::setDescription(const std::string &newdesc){
-  description = newdesc;
+  info->setDescription(newdesc);
   touchModTime();
 }
 
 void IGObject::addToParent(uint32_t pid){
-    parentid = pid;
-    myGame->getObjectManager()->getObject(parentid)->addContainedObject(id);
-    myGame->getObjectManager()->doneWithObject(parentid);
+    relationships->setParent(pid);
+    Game::getGame()->getObjectManager()->getObject(pid)->addContainedObject(id);
+    Game::getGame()->getObjectManager()->doneWithObject(pid);
     touchModTime();
 }
 
-void IGObject::removeFromParent()
-{
-  myGame->getObjectManager()->getObject(parentid)->removeContainedObject(id);
-    myGame->getObjectManager()->doneWithObject(parentid);
-  parentid = 0;
+void IGObject::removeFromParent(){
+  Game::getGame()->getObjectManager()->getObject(relationships->getParent())->removeContainedObject(id);
+  Game::getGame()->getObjectManager()->doneWithObject(relationships->getParent());
+  relationships->setParent(0);
   touchModTime();
 }
 
 int IGObject::getContainerType(){
-  return myObjectData->getContainerType();
-}
-
-std::set<unsigned int> IGObject::getContainedObjects()
-{
-	return children;
-}
-
-bool IGObject::addContainedObject(unsigned int addObjectID)
-{
-  if(myObjectData->getContainerType() >= 1){
-	//check that the new object is *really* inside the object
-  
-        //Game::getGame()->getObject(addObjectID)->parentid = id;
-	touchModTime();
-	return children.insert(addObjectID).second;
+  if(behaviour != NULL){
+    return behaviour->getContainerType();
   }
-  return false;
+  return 0;
 }
 
-bool IGObject::removeContainedObject(unsigned int removeObjectID)
-{
-	// remove object
-	unsigned int currsize = children.size();
-	touchModTime();
-	return children.erase(removeObjectID) == currsize - 1;
+std::set<uint32_t> IGObject::getContainedObjects(){
+  return relationships->getChildren();
 }
 
-void IGObject::createFrame(Frame * frame, int playerid)
-{
+void IGObject::addContainedObject(uint32_t addObjectID){
+  if(getContainerType() >= 1){
+    //check that the new object is *really* inside the object
+
+    //Game::getGame()->getObject(addObjectID)->parentid = id;
+    touchModTime();
+    relationships->addChild(addObjectID);
+  }
+}
+
+void IGObject::removeContainedObject(unsigned int removeObjectID){
+  // remove object
+  touchModTime();
+  relationships->removeChild(removeObjectID);
+}
+
+void IGObject::createFrame(Frame * frame, uint32_t playerid){
   
   frame->setType(ft02_Object);
   frame->packInt(id);
-  frame->packInt(type);
-  frame->packString(name);
+  info->packFrame(frame);
+  
   if(frame->getVersion() >= fv0_4){
-    frame->packString(description);
-    frame->packInt(parentid);
+    // should be moved to ObjectRelationshipsData::packFrame once TP03 support is removed
+    frame->packInt(relationships->getParent());
   }else{
     //pre tp04
-    if(myObjectData != NULL){
-      SizeObjectParam * size = dynamic_cast<SizeObjectParam*>(myObjectData->getParameterByType(obpT_Size));
-      if(size != NULL){
-       frame->packInt64(size->getSize());
-      }else{
-        frame->packInt64(0);
-      }
+   
+    SizeObjectParam * size = dynamic_cast<SizeObjectParam*>(getParameterByType(obpT_Size));
+    if(size != NULL){
+      frame->packInt64(size->getSize());
     }else{
       frame->packInt64(0);
     }
-    if(myObjectData != NULL){
-      Position3dObjectParam * pos = dynamic_cast<Position3dObjectParam*>(myObjectData->getParameterByType(obpT_Position_3D));
-      if(pos != NULL){
-        pos->getPosition().pack(frame);
-      }else{
-        Vector3d(0,0,0).pack(frame);
-      }
+  
+    Position3dObjectParam * pos = dynamic_cast<Position3dObjectParam*>(getParameterByType(obpT_Position_3D));
+    if(pos != NULL){
+      pos->getPosition().pack(frame);
     }else{
       Vector3d(0,0,0).pack(frame);
     }
-    if(myObjectData != NULL){
-      Velocity3dObjectParam * vel = dynamic_cast<Velocity3dObjectParam*>(myObjectData->getParameterByType(obpT_Velocity));
-      if(vel != NULL){
-        vel->getVelocity().pack(frame);
-      }else{
-        Vector3d(0,0,0).pack(frame);
-      }
+  
+    Velocity3dObjectParam * vel = dynamic_cast<Velocity3dObjectParam*>(getParameterByType(obpT_Velocity));
+    if(vel != NULL){
+      vel->getVelocity().pack(frame);
     }else{
       Vector3d(0,0,0).pack(frame);
     }
+  
   }
  
-  std::set<unsigned int> temp = children;
-  std::set < unsigned int >::iterator itcurr, itend;
-  itcurr = temp.begin();
-  itend = temp.end();
-  Player* player = Game::getGame()->getPlayerManager()->getPlayer(playerid);
-  while(itcurr != itend){
-    if(!player->getPlayerView()->isVisibleObject(*itcurr)){
-      std::set<unsigned int>::iterator itemp = itcurr;
-      ++itcurr;
-      temp.erase(itemp);
-    }else{
-      ++itcurr;
-    }
-  }
-
-  frame->packInt(temp.size());
-  //for loop for children objects
-  itend = temp.end();
-  for (itcurr = temp.begin(); itcurr != itend; itcurr++) {
-    frame->packInt(*itcurr);
-  }
+  relationships->packFrame(frame, playerid);
 
   if(frame->getVersion() <= fv0_3){
   
-    if(myObjectData != NULL){
-      OrderQueueObjectParam* oq = dynamic_cast<OrderQueueObjectParam*>(myObjectData->getParameterByType(obpT_Order_Queue));
-      if(oq != NULL){
-        OrderQueue* queue = Game::getGame()->getOrderManager()->getOrderQueue(oq->getQueueId());
-        if(queue->isOwner(playerid)){
-          std::set<uint32_t> allowedtypes = queue->getAllowedOrderTypes();
-          frame->packInt(allowedtypes.size());
-          for(std::set<uint32_t>::iterator itcurr = allowedtypes.begin(); itcurr != allowedtypes.end();
-              ++itcurr){
-            frame->packInt(*itcurr);
-          }
-          frame->packInt(queue->getNumberOrders());
-        }else{
-          frame->packInt(0);
-          frame->packInt(0);
+    
+    OrderQueueObjectParam* oq = dynamic_cast<OrderQueueObjectParam*>(getParameterByType(obpT_Order_Queue));
+    if(oq != NULL){
+      OrderQueue* queue = Game::getGame()->getOrderManager()->getOrderQueue(oq->getQueueId());
+      if(queue->isOwner(playerid)){
+        std::set<uint32_t> allowedtypes = queue->getAllowedOrderTypes();
+        frame->packInt(allowedtypes.size());
+        for(std::set<uint32_t>::iterator itcurr = allowedtypes.begin(); itcurr != allowedtypes.end();
+            ++itcurr){
+          frame->packInt(*itcurr);
         }
+        frame->packInt(queue->getNumberOrders());
       }else{
         frame->packInt(0);
         frame->packInt(0);
       }
-      
     }else{
       frame->packInt(0);
       frame->packInt(0);
     }
+      
   }
   //padding
   if(frame->getVersion() >= fv0_3){
-    frame->packInt64(myObjectData->getModTime());
+    frame->packInt64(getModTime());
     frame->packInt(0);
     frame->packInt(0);
     if(frame->getVersion() >= fv0_4){
@@ -282,53 +229,54 @@ void IGObject::createFrame(Frame * frame, int playerid)
     frame->packInt(0);
   }
   if(frame->getVersion() >= fv0_4){
-    myObjectData->packObjectParameters(frame, playerid);
+    for(std::map<uint32_t, ObjectParameterGroupPtr>::iterator itcurr = parameters.begin(); itcurr != parameters.end();
+      ++itcurr){
+    (itcurr->second)->packObjectFrame(frame, playerid);
+  }
   }else{
-    if(myObjectData != NULL){
-      myObjectData->packExtraData(frame);
+    if(behaviour != NULL){
+      behaviour->packExtraData(frame);
     }
   }
 
 }
 
-ObjectData* IGObject::getObjectData(){
-  return myObjectData;
+ObjectBehaviour* IGObject::getObjectBehaviour() const{
+  return behaviour;
 }
 
 void IGObject::touchModTime(){
-  if(myObjectData != NULL)
-    myObjectData->touchModTime();
+  modtime = time(NULL);
 }
 
-long long IGObject::getModTime() const{
-  if(myObjectData != NULL)
-    return myObjectData->getModTime();
-  else
-    return 0LL;
+uint64_t IGObject::getModTime() const{
+  uint64_t maxmodtime = modtime;
+  if(info->getModTime() > maxmodtime){
+    maxmodtime = info->getModTime();
+  }
+  
+  return maxmodtime;
 }
 
-bool IGObject::isDirty() const{
-  if(myObjectData != NULL)
-    return myObjectData->isDirty();
-  else
-    return true;
-}
 
 void IGObject::setParent(uint32_t pid){
-    parentid = pid;
+  relationships->setParent(pid);
 }
 
 void IGObject::setModTime(uint64_t time){
-    if(myObjectData != NULL)
-        myObjectData->setModTime(time);
+  modtime = time;
 }
 
-void IGObject::setDirty(bool nd){
-  if(myObjectData != NULL)
-    myObjectData->setDirty(nd);
-}
-
-void IGObject::signalRemoval(){
-  if(myObjectData != NULL)
-    myObjectData->signalRemoval();
+ObjectParameter* IGObject::getParameterByType(uint32_t ptype) const{
+  for(std::map<uint32_t, ObjectParameterGroupPtr>::const_iterator itcurr = parameters.begin(); itcurr != parameters.end();
+      ++itcurr){
+    std::list<ObjectParameter*> params = (itcurr->second)->getParameters();
+    for(std::list<ObjectParameter*>::iterator opitcurr = params.begin(); opitcurr != params.end();
+        ++opitcurr){
+      if((*opitcurr)->getType() == ptype){
+        return (*opitcurr);
+      }
+    }
+  }
+  return NULL;
 }
