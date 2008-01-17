85e4c6c9caf42c84b57577ab692ce88e7ce207c8
Lee Begg <llnz@paradise.net.nz>
2008-01-17 23:19:48 +1300

    Fix copyright date in objecttypemanager.cpp

--- tpserver/objecttypemanager.cpp
+++ tpserver/objecttypemanager.cpp
@@ -1,6 +1,6 @@
 /*  ObjectDataManager for managing ObjectData objects
  *
- *  Copyright (C) 2003-2005, 2007  Lee Begg and the Thousand Parsec Project
+ *  Copyright (C) 2003-2005, 2007, 2008  Lee Begg and the Thousand Parsec Project
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
