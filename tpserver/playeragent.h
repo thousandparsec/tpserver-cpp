4c8025f384c3fd0c04593ac782c7954888f13850
Lee Begg <llnz@paradise.net.nz>
2008-05-03 11:51:13 +1200

    Implemented PlayerIds_Get incoming frames. Only TP04.

--- tpserver/playeragent.h
+++ tpserver/playeragent.h
@@ -2,7 +2,7 @@
 #define PLAYERAGENT_H
 /*  PlayerAgent class
  *
- *  Copyright (C) 2007  Lee Begg and the Thousand Parsec Project
+ *  Copyright (C) 2007, 2008  Lee Begg and the Thousand Parsec Project
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -69,6 +69,7 @@ private:
   void processGetResourceTypes(Frame* frame);
 
   void processGetPlayer(Frame* frame);
+  void processGetPlayerIds(Frame* frame);
 
   void processGetCategory(Frame* frame);
   void processGetCategoryIds(Frame* frame);
