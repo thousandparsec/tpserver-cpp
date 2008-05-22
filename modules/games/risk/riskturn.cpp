d755ce135946b36a5416d1c3d40e09f56109b1d6
Ryan Neufeld <jphr@hammerofcode.com>
2008-05-22 16:27:33 -0500

    Create riskturn.cpp&.h as well as a "settings" file   -RiskTurn inherits TurnProcess and is only a shell at the moment   -settings records settings i've used and settings in consideration

---
+++ modules/games/risk/riskturn.cpp
@@ -0,0 +1,58 @@
+/*  RiskTurn object
+ *
+ *  Copyright (C) 2007, 2008  Lee Begg and the Thousand Parsec Project
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+#include <algorithm>
+
+#include <tpserver/game.h>
+#include <tpserver/ordermanager.h>
+#include <tpserver/objectmanager.h>
+#include <tpserver/playermanager.h>
+#include <tpserver/order.h>
+#include <tpserver/object.h>
+#include <tpserver/objectview.h>
+#include <tpserver/player.h>
+#include <tpserver/playerview.h>
+#include <tpserver/objecttypemanager.h>
+#include <tpserver/objectparameter.h>
+#include <tpserver/orderqueueobjectparam.h>
+#include <tpserver/orderqueue.h>
+#include <tpserver/orderqueueobjectparam.h>
+#include <tpserver/orderqueue.h>
+#include <tpserver/ordermanager.h>
+#include <tpserver/message.h>
+#include <stdint.h>
+
+#include "riskturn.h"
+
+namespace RiskRuleset{
+
+RiskTurn::RiskTurn() : TurnProcess(), /*containerids()*/{
+    //minisec has containerids() as a parent, look into this
+} 
+    
+RiskTurn::~RiskTurn(){
+    
+}
+
+void RiskTurn::doTurn(){
+    //turn processing goes here
+    
+} //RiskTurn::RiskTurn() : TurnProcess()
+} //namespace RiskRuleset
