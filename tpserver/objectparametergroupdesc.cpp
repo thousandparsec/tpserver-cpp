138206c6edf7411df48d79df5276fdab00a19981
Lee Begg <llnz@paradise.net.nz>
2009-05-27 22:10:29 +1200

    Added Media ObjectParameter to tpserver-cpp. Had not yet been added. Needs users in the rulesets.

--- tpserver/objectparametergroupdesc.cpp
+++ tpserver/objectparametergroupdesc.cpp
@@ -1,6 +1,6 @@
 /*  ObjectParameterGroup class
  *
- *  Copyright (C) 2007, 2008 Lee Begg and the Thousand Parsec Project
+ *  Copyright (C) 2007, 2008, 2009 Lee Begg and the Thousand Parsec Project
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -30,6 +30,7 @@
 #include "referenceobjectparam.h"
 #include "refquantitylistobjectparam.h"
 #include "integerobjectparam.h"
+#include "mediaobjectparam.h"
 #include "logging.h"
 
 #include "objectparametergroupdesc.h"
@@ -165,6 +166,9 @@ ObjectParameterGroupPtr ObjectParameterGroupDesc::createObjectParameterGroup() c
         break;
       case obpT_Size:
         param = new SizeObjectParam();
+        break;
+    case obpT_Media:
+        param = new MediaObjectParam();
         break;
         
       default:
