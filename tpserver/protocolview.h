f14c9bf76880ebdbf9d1daffcd08fba50473c177
Kornel Kisielewicz <epyon@neko.dnsalias.net>
2009-07-03 00:26:01 +0200

    ProtocolView base class added

---
+++ tpserver/protocolview.h
@@ -0,0 +1,47 @@
+#ifndef PROTOCOLVIEW_H
+#define PROTOCOLVIEW_H
+/*  ProtocolView base class
+ *
+ *  Copyright (C) 2009 Kornel Kisielewicz and the Thousand Parsec Project
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+#include <tpserver/protocolobject.h>
+
+class ProtocolView : public ProtocolObject {
+  public:
+    ProtocolView( FrameType frame_type ) : ProtocolObject( frame_type, 0, "", "" ) {}
+    virtual ~ProtocolView() {}
+    void pack(Frame* frame) const {};
+    bool isCompletelyVisible() const { return completely_visible; }
+    bool canSeeName() const { return name_visible; }
+    std::string getVisibleName() const { return name; }
+    bool canSeeDescription() const { return desc_visible; }
+    std::string getVisibleDescription() const { return desc; }
+
+    void setCompletelyVisible(bool visibility) { completely_visible = visibility; }
+    void setCanSeeName(bool visibility) { name_visible = visibility; }
+    void setVisibleName(const std::string& new_name) { setName( new_name ); }
+    void setCanSeeDescription(bool visibility) { desc_visible = visibility; }
+    void setVisibleDescription(const std::string& new_desc) { setDescription( new_desc ); }
+  protected:
+    bool completely_visible;
+    bool name_visible;
+    bool desc_visible;
+};
+
+#endif // PROTOCOLOBJECT_H
